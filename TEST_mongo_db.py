a148ec8eccef1ddad5b1705f5f23f6c8bfeb80f9
VENKATCHINTHAKINDI <venkatchinthakindi4@gmail.com>
2025-12-22 20:14:53 +0530

    end to end

--- TEST_mongo_db.py
+++ TEST_mongo_db.py
@@ -2,7 +2,7 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://venkatstark5_db_user:venkat123@cluster0.y2o3w1m.mongodb.net/?appName=Cluster0"
+uri = 
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
